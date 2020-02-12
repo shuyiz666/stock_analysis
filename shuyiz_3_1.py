54cbe7e920c5a9ba7451af659c5b62b44b4785f1
shuyizheng <zhengs5@uni.coventry.ac.uk>
2020-02-12 15:35:15 -0500

    update

--- shuyiz_3_1.py
+++ shuyiz_3_1.py
@@ -14,15 +14,11 @@ ticker_file = os.path.join(input_dir, ticker + '_label.csv')
 try:
     df = pd.read_csv(ticker_file)
     money = 100
-    # 0 only have money 1 only have stock
+    # flag = 0 only have money 1 only have stock
     flag = 0
     value = 100
     values = []
     values_hold = []
-    # red green 买 有股票没钱
-    # green red 卖 有钱没股票
-    # green green 不动 有股票没钱
-    # red red 不动 有钱没股票
     x = 0
     for index, row in df.iterrows():
         # buy hold strategy
@@ -32,6 +28,8 @@ try:
             stock = 100/row['Adj Close']
         else:
             values_hold.append(stock*row['Adj Close'])
+
+        # trading by labels
         # red to green, buy stock
         if row['label'] == 'green' and flag == 0:
             shares = money/row['Adj Close']
@@ -46,21 +44,21 @@ try:
             value = shares*row['Adj Close']
         # green to red, sell stock
         elif row['label'] == 'green' and flag == 1:
-            money = shares/row['Adj Close']
+            money = shares*row['Adj Close']
             shares = 0
             value = money
         values.append(value)
-    plt.figure(figsize=(10, 5))
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111)
     plt.title('')
     plt.xlabel('date', fontsize=14)
-    plt.ylabel('value', fontsize=14)
-    plt.plot(df['Date'],values)
-    plt.plot(df['Date'],values_hold)
+    xlabels = df['Year'].map(str)+'/'+df['Week_Number'].map(str)
+    ax.axes.set_xticklabels(xlabels, rotation=90,fontsize=5)
+    plt.ylabel('stock holding price', fontsize=14)
+    plt.plot(xlabels,values)
+    plt.plot(df['Year'].map(str)+'/'+df['Week_Number'].map(str),values_hold)
     plt.legend(['trading by label','buy_hold'])
     plt.show()
-
-
-
 
 except Exception as e:
     print(e)
