6f9cab0bbcb8da78ec77b359a5b3fc124d1f8d2e
shuyizheng <zhengs5@uni.coventry.ac.uk>
2020-02-13 22:28:18 -0500

    update

--- shuyiz_3_3_7.py
+++ shuyiz_3_3_7.py
@@ -2,3 +2,20 @@
 assignment3: bakery dataset
 question7:  what are the top 5 most popular items for each day of the week
 '''
+import os
+import pandas as pd
+
+wd = os.getcwd()
+input_dir = wd
+ticker_file = os.path.join(input_dir, 'BreadBasket_DMS_output.csv')
+
+try:
+    df = pd.read_csv(ticker_file)
+    result = df.groupby(['Weekday'])['Item'].value_counts().rename('count').reset_index()
+    print(result.groupby('Weekday').head(5),'\n')
+    print('Coffee, bread, tea are top3 every day of the week.')
+    print('')
+
+except Exception as e:
+    print(e)
+    print('failed to read data')
