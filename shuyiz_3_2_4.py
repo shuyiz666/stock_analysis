107b28146c7822bc1993aab2004072463b5f0bef
shuyizheng <zhengs5@uni.coventry.ac.uk>
2020-02-17 14:38:23 -0500

    update

--- shuyiz_3_2_4.py
+++ shuyiz_3_2_4.py
@@ -3,5 +3,7 @@ assignment2: retail bernford
 question4: discuss
 '''
 print('The frequencies for real distribution and Bernford have the same trend. The frequency goes down when digit becomes bigger')
-print('The relative errors for Model 1 are all smaller than Model 2.')
+print('The relative errors for Model 1 compared with real distribution has the same trend with it compared with Bernford distribution.')
+print('digit 4 has the smallest relatives for model 1.')
+print('The relative errors for Model 2 compared with equal-weight are very big when digits are 8 and 9.')
 print('The RMSE for Model 1 is bigger than Model 2.')
