81b1a80986efcbb977a30420263a7aa794dd6849
shuyizheng <zhengs5@uni.coventry.ac.uk>
2020-02-26 22:18:45 -0500

    update

--- shuyiz_4_2_1.py
+++ shuyiz_4_2_1.py
@@ -23,26 +23,29 @@ x = 0
 for index, row in df.iterrows():
     # trading by labels
     # red to green, buy stock
+    # red to green, buy stock
     if row['label'] == 'green' and flag == 0:
-        shares = money/row['Adj Close']
+        shares = money / row['Adj Close']
         money = 0
         flag = 1
-        value = shares*row['Adj Close']
+        value = shares * row['Adj Close']
     # green to green, do nothing
     elif row['label'] == 'green' and flag == 1:
-        value = shares*row['Adj Close']
+        value = shares * row['Adj Close']
     # red to red, do nothing
     elif row['label'] == 'red' and flag == 0:
-        value = shares*row['Adj Close']
+        value = value
     # green to red, sell stock
-    elif row['label'] == 'green' and flag == 1:
-        money = shares*row['Adj Close']
+    elif row['label'] == 'red' and flag == 1:
+        money = shares * row['Adj Close']
         shares = 0
+        flag = 0
         value = money
     if row['Year'] == 2017:
         values2017.append(value)
     elif row['Year'] == 2018:
         values2018.append(value)
+
 print('2017: ')
 print('the average of weekly balances is ',round(st.mean(values2017),2))
 print('the volatility of weekly balances is ',round(st.stdev(values2017),2),'\n')
