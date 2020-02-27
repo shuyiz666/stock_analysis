eaebf0bbecd73a5b00c885a646cd12495f9672bf
shuyizheng <zhengs5@uni.coventry.ac.uk>
2020-02-26 22:58:21 -0500

    update

--- shuyiz_5_3_3.py
+++ shuyiz_5_3_3.py
@@ -5,6 +5,7 @@ question3: implement a trading strategy based on your labels for year 2
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 wd = os.getcwd()
 ticker = 'ZSAN'
@@ -22,27 +23,42 @@ for index, row in df.iterrows():
     else:
         df.loc[index,'predict_label'] = 'red'
 
+# trading by predict_labels
+money = 100
+# flag = 0 only have money 1 only have stock
+flag = 0
+value = 100
+values_hold = []
+x = 0
 for index, row in df.iterrows():
-    # trading by labels
     # red to green, buy stock
-    if row['label'] == 'green' and flag == 0:
-        shares = money/row['Adj Close']
+    if row['predict_label'] == 'green' and flag == 0:
+        shares = money / row['Adj Close']
         money = 0
         flag = 1
-        value = shares*row['Adj Close']
+        value = shares * row['Adj Close']
     # green to green, do nothing
-    elif row['label'] == 'green' and flag == 1:
-        value = shares*row['Adj Close']
+    elif row['predict_label'] == 'green' and flag == 1:
+        value = shares * row['Adj Close']
     # red to red, do nothing
-    elif row['label'] == 'red' and flag == 0:
-        value = shares*row['Adj Close']
+    elif row['predict_label'] == 'red' and flag == 0:
+        value = value
     # green to red, sell stock
-    elif row['label'] == 'green' and flag == 1:
-        money = shares*row['Adj Close']
+    elif row['predict_label'] == 'red' and flag == 1:
+        money = shares * row['Adj Close']
         shares = 0
+        flag = 0
         value = money
-    plt.title('growth of balance in '+str(year))
-    plt.xlabel('week numbers')
-    plt.ylabel('stock holding price')
-    plt.plot(df['Week_Number'],values)
-    plt.show()
+    values_hold.append(value)
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+fmt = '${x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+ax.yaxis.set_major_formatter(tick)
+plt.title('trading with predict labels in 2018')
+plt.xlabel('week_number', fontsize=14)
+xlabels = df['Week_Number']
+plt.ylabel('portfolio', fontsize=14)
+plt.yticks()
+plt.plot(xlabels,values_hold)
+plt.show()
