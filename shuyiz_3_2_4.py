0f45536da4f290af9c76cbc98dc291326075edd6
shuyiz666 <58002720+shuyiz666@users.noreply.github.com>
2020-03-28 15:03:04 -0400

    discussion

--- shuyiz_3_2_4.py
+++ shuyiz_3_2_4.py
@@ -6,4 +6,4 @@ print('The frequencies for real distribution and Bernford have the same trend. T
 print('The relative errors for Model 1 compared with real distribution has the same trend with it compared with Bernford distribution.')
 print('digit 4 has the smallest relatives for model 1.')
 print('The relative errors for Model 2 compared with equal-weight are very big when digits are 8 and 9.')
-print('The RMSE for Model 1 is bigger than Model 2.')
+print('The RMSE for Model 1 is bigger than Model 2.') 
