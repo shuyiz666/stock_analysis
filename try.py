6f6ffd229de37a68ec2a368f83377bbfb2d7c390
shuyizheng <zhengs5@uni.coventry.ac.uk>
2020-03-28 13:50:45 -0400

    update

--- try.py
+++ try.py
@@ -1,15 +1,9 @@
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-from sklearn.metrics import mean_squared_error,r2_score
-x = np.array([1, 2, 3, 4, 6, 8])
-y = np.array([1, 1, 5, 8, 3, 5])
-degree = 2
-weights = np.polyfit(x,y, degree)
-model = np.poly1d(weights)
-predicted = model(x)
-print(y)
-r2 = r2_score(np.array([1]),np.array([2]))
-print(r2)
+import math
+
+df = pd.read_csv('jia_mix.csv')
+print(df.iloc[:,1:23])
+df.iloc[:,1:24] = df.iloc[:,1:24].apply(lambda x:x if x==0 else np.log(x))
+print(df)
