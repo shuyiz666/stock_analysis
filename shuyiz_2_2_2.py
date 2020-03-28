8aedc0261068273973f7880a238bb02b31b92ce3
shuyiz666 <58002720+shuyiz666@users.noreply.github.com>
2020-03-28 14:49:53 -0400

    highest profit per transaction

--- shuyiz_2_2_2.py
+++ shuyiz_2_2_2.py
@@ -1,8 +1,9 @@
 '''
 assigment 2: Bollinger bands
-question 3: repeat the previous question for 2018
+question 2: highest profit per transaction
 '''
 print('When the W becomes bigger, the profit beacomes higher.')
 print('It seems that at k = 1 and k = 2.5, the profit is higher than others.')
 print('There is no loss with any W and k.')
 print('The highest profit per transaction in 2017 is 298.98 when W from 31 to 36, k=1.') 
+ 
