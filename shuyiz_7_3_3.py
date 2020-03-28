f03c35ab75d356ca26e898f13ce6d36386898505
shuyiz666 <58002720+shuyiz666@users.noreply.github.com>
2020-03-28 15:54:11 -0400

    Change of Trend Detectio

--- shuyiz_7_3_3.py
+++ shuyiz_7_3_3.py
@@ -2,4 +2,4 @@
 assignment1: Change of Trend Detection
 question3: are there more ”changes” in year 1 or in year 2?
 '''
-print('the changes in year 1 and year 2 are same')
+print('the changes in year 1 and year 2 are same') 
